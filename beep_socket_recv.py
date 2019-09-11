978da6f0812bb3a721d953acb52c3adca94980e9
Jacob <jacobpierce@uchicago.edu>
2019-09-11 14:10:36 -0700

    beep signal now sent

--- beep_socket_recv.py
+++ beep_socket_recv.py
@@ -1,18 +1,36 @@
 #!/usr/bin/env python3
 
 import socket
+import os 
+import time 
+
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 2001        # Port to listen on (non-privileged ports are > 1023)
+PORT = int( os.environ[ 'BEEP_SOCKET_RECV_PORT' ] )     
+
+
+BEEP_SCRIPT = print(os.path.dirname(os.path.realpath(__file__))) + '/beep.sh'
+
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
     conn, addr = s.accept()
     with conn:
+        
         print('Connected by', addr)
+        
         while True:
+        
             data = conn.recv(1024)
+
+            print( data )
+
+            if data == 'beep' : 
+                os.system( BEEP_SCRIPT )
+        
             if not data:
-                break
+                time.sleep(1)
+        
             conn.sendall(data)
--- beep_socket_send.py
+++ beep_socket_send.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
 import socket
+import os 
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 2000        # The port used by the server
+PORT = int( os.environ[ 'BEEP_SOCKET_SEND_PORT' ] )       # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
+    s.sendall(b'beep')
+    # data = s.recv(1024)
 
 print('Received', repr(data))
