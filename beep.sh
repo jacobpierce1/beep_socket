776a88353f37a936c873be0b830bb5459610c4e8
Jacob <jacobpierce@uchicago.edu>
2020-10-21 14:17:24 -0700

    change sound effect and volume

--- beep.sh
+++ beep.sh
@@ -2,4 +2,4 @@
 
 # feel free to customize this, there are other sounds available in the Sounds directory on macos. you can easily find an appropriate substituion for linux. 
 
-afplay -v 4 -q 1 /System/Library/Sounds/Ping.aiff 
+afplay -v 1 -q 1 /System/Library/Sounds/Glass.aiff 
